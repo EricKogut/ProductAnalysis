b8e8fb2eb8b5b8398ee28ad02a1ea72e134c1c8b
EricKogut <40190283+EricKogut@users.noreply.github.com>
2019-08-06 12:26:18 -0400

    Update Plotly_Graphing.py

--- Plotly_Graphing.py
+++ Plotly_Graphing.py
@@ -1,3 +1,5 @@
+
+#These will be used in order to display the graphs
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly
@@ -5,19 +7,23 @@ import Product
 
 import Product_Compare
 
-plotly.tools.set_credentials_file(username='Eric_Kogut', api_key='zf6Wh20MnEQ6yl577x3p')
+plotly.tools.set_credentials_file(username='uhhhhhh', api_key='its a secret')
 
+#Hardcoded comparisons
 product_list_one = Product.CreateProductList("Watches 26-06-19.csv")
 product_list_two = Product.CreateProductList("Watches 18-06-19.csv")
 Product_Compare.checkProducts(product_list_one, product_list_two)
 
 
+#Setting the parameters
 y_axis = []
 x_axis = []
 opacity = []
 size = []
 colour = []
 counter = 0
+
+#Populating the y and x axis lists
 for element in range(len(product_list_one)):
     if ((product_list_one[element].quantity_change != 0)):
         y_axis.append(product_list_one[element].quantity_change)
@@ -25,13 +31,12 @@ for element in range(len(product_list_one)):
         size.append(40)
         opacity.append(1)
 
-
     #print(size[element])
     colour.append('rgb(93, 164, 214)')
-
     counter= counter+1
 
 
+#Creating the traces to be displayed
 trace0 = go.Scatter(
     x=x_axis,
     y=y_axis,
@@ -42,5 +47,6 @@ trace0 = go.Scatter(
     )
 )
 
+#Sending the data to plotly
 data = [trace0]
 py.plot(data, filename='multiple-annotation')
